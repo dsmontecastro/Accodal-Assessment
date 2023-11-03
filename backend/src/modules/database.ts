cbfcee428356fcbce0757e79c3af9df543b5455a
D2 <dsmontecastro1@up.edu.ph>
2023-11-03 23:17:43 +0800

    [BACK] Tentatively finalized

--- backend/src/modules/database.ts
+++ backend/src/modules/database.ts
@@ -1,11 +1,19 @@
+/**
+ * Readies and holds the <database> instance.
+*/
+
 import Database from 'better-sqlite3';
 
 const MAKE_ITEMS = 'CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY, name TEXT, owner TEXT, desc TEXT);';
 
-function startDB() {
+function startDB(path: string = './database.db') {
+    /**
+     * Start the Database, and prepare it for exporting.
+     * @param {string} [path = './database.db'] Path to the <database> file.
+     * @return {Database.Database} Returns a <database> with an Items-Table ready.
+    */
 
-    const db = new Database('./database.db', { verbose: console.log });
-
+    const db = new Database(path);
     db.pragma('journal_mode = WAL');
     db.exec(MAKE_ITEMS);
 
