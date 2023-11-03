a04cb48eb4bd49f2e35572e167deda272911485d
D2 <dsmontecastro1@up.edu.ph>
2023-11-03 17:57:51 +0800

    [FRONT] Tentatively finished

--- frontend/src/modules/index.tsx
+++ frontend/src/modules/index.tsx
@@ -1,21 +1,13 @@
-// import { createClient } from 'pexels';
-
-import { TimeAPI } from "./time/api";
+import TimeAPI from "./time/api";
+import PexelsAPI from "./pexels/api";
 
 
 function App() {
 
-  // const [count, setCount] = useState(0)
-
-  return (
-    <>
-
-      <TimeAPI />
-
-      <a href="https://www.pexels.com"> Photos provided by Pexels </a>
-
-    </>
-  )
+  return <>
+    <TimeAPI />
+    <PexelsAPI />
+  </>
 
 }
 
