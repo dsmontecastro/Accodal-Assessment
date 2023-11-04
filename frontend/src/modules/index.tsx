020ade9d69e3f29e3136f2273e8fc9e9a58d5dde
D2 <dsmontecastro1@up.edu.ph>
2023-11-04 15:40:15 +0800

    [-] Minor changes

--- frontend/src/modules/index.tsx
+++ frontend/src/modules/index.tsx
@@ -8,13 +8,17 @@ export default function App() {
   */
 
   return (
+
     <div id='app' className='flex flex-col
       w-full h-full
       portrait:flex-row portrait:min-h-[100%]
     '>
+
       <TimeAPI />
       <PexelsAPI />
+
     </div>
+
   )
 
 }
